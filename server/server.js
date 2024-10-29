fb4ad4c409feb207863ca793e65bbb63b97f7292
BrianGates <u22516728@tuks.co.za>
2024-10-29 21:24:41 +0200

    Getting api to work deployed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://BrianGates101.github.io/Garrie-Spider-Website/client/garrie-website'],
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 const PORT = process.env.PORT || 5000;
 
 app.get('/api', (req, res) => {
