a400ae61d8fbe6c4352b38f11839282db6adbc45
BrianGates <u22516728@tuks.co.za>
2024-10-29 21:33:11 +0200

    This...

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// Update allowed origins for CORS
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://BrianGates101.github.io/Garrie-Spider-Website/client/garrie-website'],
-    optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+    origin: [
+      'http://localhost:5173', // local development
+      'https://briangates101.github.io' // GitHub Pages
+    ],
+    optionsSuccessStatus: 200, // For legacy browsers
+  };
+  
+  // Use CORS middleware with specified options
+  app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 
