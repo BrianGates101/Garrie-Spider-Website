42bb5de92632e6da5b47de30e235c297c38f9777
BrianGates <u22516728@tuks.co.za>
2024-10-29 21:44:22 +0200

    Another commit...

--- client/garrie-website/vite.config.js
+++ client/garrie-website/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Garrie-Spider-Website/client/garrie-website/', // Set this to match your repository path
 })
